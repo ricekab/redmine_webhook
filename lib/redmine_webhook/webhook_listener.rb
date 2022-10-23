f4dea02b13437825194cdeb1f69b93f267b2aca8
ricekab <contact@kevinchiyantang.com>
2022-10-23 14:26:21 +0200

    Added secret key to webhook model for HMAC verification. HMAC generation is not added yet.

--- lib/redmine_webhook/webhook_listener.rb
+++ lib/redmine_webhook/webhook_listener.rb
@@ -78,9 +78,15 @@ module RedmineWebhook
       Thread.start do
         webhooks.each do |webhook|
           begin
+            # HMAC here
+            #key = webhook.secret_key
+            #data = request_body  # Might need to parse to string? not sure what type this is
+            #mac = OpenSSL::HMAC.hexdigest("SHA1", key, data)
+            # ---
             Faraday.post do |req|
               req.url webhook.url
               req.headers['Content-Type'] = 'application/json'
+              # req.headers['X-RedmineWebhook-Signature'] = mac
               req.body = request_body
             end
           rescue => e
